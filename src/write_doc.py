c565bc09b73600b05d814018177493dff65aeed6
Cooper Barth <cooperfbarth@gmail.com>
2019-06-25 20:56:26 -0500

    writes to docx file

--- src/write_doc.py
+++ src/write_doc.py
@@ -4,20 +4,21 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 FONT = "Courier"
+FONT_SIZE = 5.0
+LINE_SPACING = 1.0
 
-def writeDoc(text_list, write_directory):
+def writeDoc(text_list, write_path):
     document = Document()
+    font = document.styles['Normal'].font
+    font.name = FONT
+    font.size = Pt(FONT_SIZE)
+
     for line in text_list:
         p = document.add_paragraph()
         for char in line:
             p.text += char
-        
-        run = p.add_run()
-        font = run.font
-        font.name = FONT
-        font.size = Pt(2)
 
         p_format = p.paragraph_format
         p_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        p_format.line_spacing = 1.0
-    document.save(f"{write_directory}/texterize.docx")
+        p_format.line_spacing = LINE_SPACING
+    document.save(write_path)
--- texterize/__init__.py
+++ texterize/__init__.py
@@ -1,14 +1,15 @@
-import numpy as np, math, sys
+import numpy as np
+import sys, os
 
 sys.path.append("./../src")
 from write_doc import writeDoc
 from build_block import buildBlock
 
-OUTPUT_DIRECTORY = "../test/output_files/"
+OUTPUT_DIRECTORY = "../test/output_files/texterize.docx"
 TEST_PATH = "../test/test_files/"
 
 #DESCRIPTION HERE
-def create(text, img, writePath="./"):
+def create(text, img, writePath=OUTPUT_DIRECTORY, overwrite=True):
     '''
     params:
     -text: The text to be converted into an image, in string format
@@ -17,11 +18,14 @@ def create(text, img, writePath="./"):
     '''
     assert isinstance(text, str), f"Expected input of type string, found {type(text)}."
 
+    if overwrite and os.path.exists(writePath):
+        os.remove(writePath)
+
     text_block = buildBlock(text)
     writeDoc(text_block, writePath)
     
 #DESCRIPTION HERE
-def createFromFile(filePath, img, writePath=""):
+def createFromFile(filePath, img, writePath="", overwrite=True):
     '''
     params:
     -text: TODO
@@ -31,6 +35,7 @@ def createFromFile(filePath, img, writePath=""):
     f = open(filePath, "r")
     text = f.read()
     f.close() #expand function this way so the file closes even if an error in rendering occurs
-    create(text, img, writePath)
+    create(text, img, writePath, overwrite)
 
+#TODO: Remove this; just for testing
 createFromFile(TEST_PATH + "test_1.txt", "", OUTPUT_DIRECTORY)
