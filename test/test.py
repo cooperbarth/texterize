275419110f82a20505a9e6d74cd9d34087f02c89
Cooper Barth <cooperfbarth@gmail.com>
2019-06-27 20:06:42 -0500

    Formatted html

--- test/test.py
+++ test/test.py
@@ -6,5 +6,6 @@ from __init__ import createFromFile
 
 TEST_PATH = "./test_files/"
 TEST_IMG_PATH = "./test_img/"
+FILE_TYPE = "HTML"
 
-createFromFile(TEST_PATH + "test_1.txt", TEST_IMG_PATH + "cat.jpeg")
+createFromFile(TEST_PATH + "test_1.txt", TEST_IMG_PATH + "test_1.jpg", output_file_type=FILE_TYPE)
